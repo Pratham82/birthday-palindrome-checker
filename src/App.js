69ab6adb20d10c2ad89b0097e9f4f557bc3cacb4
pratham82 <mali.prathamesh82@gmail.com>
2021-09-15 01:19:15 +0530

    Refactored code and moved to its utility functions

--- src/App.js
+++ src/App.js
@@ -1,83 +1,33 @@
 import React, { useState } from 'react'
+import { formatDate, dateVariations } from './helpers/dateUtility'
+import { getNextPalindrome, isPalindrome } from './helpers/palindromeUtility'
 
 function App() {
   const [birthday, setBirthday] = useState('')
-  const [nextPalindrome, setNextPalindrome] = useState('')
-  const [success, setSuccess] = useState(true)
+  const [nextPalindrome, setNextPalindrome] = useState({})
   const [message, setMessage] = useState('')
   const [loading, setLoading] = useState(false)
 
-  //******** Get Date variations ******/
-  const dateVariations = (formattedDate) => {
-    // Date variations
-    // DD-MM-YYYY, MM-DD-YYYY, YYYY-MM-DD, DD-MM-YY, MM-DD-YY, YY-MM-DD
-    /*
-    01012021
-
-    */
-    const joinArray = (arr) => arr.join('')
-
-    const [yyyy, mm, dd] = formattedDate.split('-')
-
-    const dateVariations = [
-      [dd, mm, yyyy],
-      [mm, dd, yyyy],
-      [yyyy, mm, dd],
-      [dd, mm, yyyy.slice(-2)],
-      [mm, dd, yyyy.slice(-2)],
-      [yyyy.slice(-2), mm, dd],
-    ].map((arr) => joinArray(arr))
-
-    return dateVariations
+  const clearRes = () => {
+    setMessage('')
+    setNextPalindrome({})
   }
 
-  const formatDate = (date) => date.toISOString().slice(0, 10)
-
-  const addDelay = (message, date) =>
+  const addDelay = (message, status, date) =>
     setTimeout(() => {
       // Stop loader
       setLoading(false)
       setMessage(message)
-      const upcomingPalindrome = getNextPalindrome(date)
-      setNextPalindrome(upcomingPalindrome)
+      if (status !== 'success') {
+        const upcomingPalindrome = getNextPalindrome(date)
+        setNextPalindrome(upcomingPalindrome)
+      }
     }, 3000)
 
-  //******* Get next date ******/
-  const getNextDate = (date) => {
-    const tomorrow = new Date(date)
-    const next = tomorrow.setDate(tomorrow.getDate() + 1)
-    return formatDate(new Date(next))
-  }
-
-  //******** Get next palindrome date and the gap in between ******/
-  const getNextPalindrome = (date) => {
-    let nextDate = getNextDate(date)
-    let days = 0
-    while (1) {
-      // Increase day count after each iteration
-      days++
-
-      // Get Date variations for next date
-      let currentDateVariations = dateVariations(nextDate)
-
-      // Check if the the date is palindrome in one of the date variations
-      let palindromeFound = currentDateVariations
-        .map((date) => isPalindrome(date))
-        .some((val) => val === true)
-
-      if (palindromeFound) {
-        console.log(nextDate)
-        break
-      } else {
-        nextDate = getNextDate(nextDate)
-      }
-    }
-    return { nextDate, days }
-  }
-
-  const isPalindrome = (bDate) => bDate === bDate.split('').reverse().join('')
-
   const checkIfPalindrome = () => {
+    // Clear prev result
+    clearRes()
+
     // If the birth date is today's date then format the date or else keep the date as is
     const finalDate = birthday instanceof Date ? formatDate(birthday) : birthday
 
@@ -99,14 +49,14 @@ function App() {
     //****** Set output *******/
     // Set palindrome or not
     if (res) {
-      setSuccess(true)
-      addDelay('Hurray your birthday is a palindrome 😄', finalDate)
+      addDelay(
+        'Hurray 🥳 your birthday is a palindrome 😄',
+        'success',
+        finalDate
+      )
     } else {
-      addDelay('Sorry your birthday is not a palindrome 😢', finalDate)
+      addDelay('Sorry 😿 your birthday is not a palindrome', 'fail', finalDate)
     }
-    // res
-    //   ? addDelay('Hurray your birthday is a palindrome 😄')
-    //   : addDelay('Sorry your birthday is not a palindrome 😢', finalDate)
   }
 
   return (
@@ -134,11 +84,8 @@ function App() {
       <div className="loadingContainer">
         <div className={`${loading ? 'loader' : ''}`}></div>
         <h2>{message}</h2>
-        <h2>
-          {!success
-            ? `Next palindrome birthday is  ${nextPalindrome.nextDate}, you just missed it by ${nextPalindrome.days} days 😞`
-            : ''}
-        </h2>
+        <h2>{nextPalindrome.nextDate}</h2>
+        <h2>{nextPalindrome.days}</h2>
       </div>
     </div>
   )
---
+++ src/helpers/palindromeUtility.js
@@ -0,0 +1,33 @@
+import { dateVariations, getNextDate } from './dateUtility'
+
+//******** Get next palindrome date and the gap in between ******/
+export const getNextPalindrome = (date) => {
+  let nextDate = getNextDate(date)
+  let days = 0
+  while (1) {
+    // Increase day count after each iteration
+    days++
+
+    // Get Date variations for next date
+    let currentDateVariations = dateVariations(nextDate)
+
+    // Check if the the date is palindrome in one of the date variations
+    let palindromeFound = currentDateVariations
+      .map((date) => isPalindrome(date))
+      .some((val) => val === true)
+
+    if (palindromeFound) {
+      break
+    } else {
+      nextDate = getNextDate(nextDate)
+    }
+  }
+  let day = days === 1 ? `1 day` : `${days} days`
+  return {
+    nextDate: `Next  palindrome date: ${nextDate}`,
+    days: `You just missed by ` + day,
+  }
+}
+
+// Check if the given date is palindrome or not
+export const isPalindrome = (date) => date === date.split('').reverse().join('')
